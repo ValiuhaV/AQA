d36d0f64da3319bf237a009d0f4c7430c5635a99
Valentyna Valiuk <valiuk.gt@gmail.com>
2025-10-12 15:33:54 +0300

    Зміни в коді відносно правок

--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,35 +1,76 @@
 //1
-
 console.log('Звичайна функція:');
+function sumArray(arr: number[] | string[]): number | string {
+    if (arr.length === 0) {
+        console.log('Масив порожній, повертаємо 0');
+        return 0;
+    }
 
-function sumArray(arr: (number | string)[]): number {
-    let sum = 0;
-    arr.forEach((element) => {
-        sum += Number(element);
+    let canConvertAllToNumbers = true;
+    arr.forEach(el => {
+        if (isNaN(Number(el))) {
+            canConvertAllToNumbers = false;
+        }
     });
+
+    if (canConvertAllToNumbers) {
+        let sum = 0;
+        arr.forEach(num => {
+            sum += Number(num);
+        });
+        console.log('Усі елементи числові. Сума елементів:', sum);
+        return sum;
+    }
+
+    let sum = '';
+    arr.forEach(num => {
+        sum += String(num);
+    });
+    console.log('Знайдено нечислові елементи. Конкатенуємо всі елементи:', sum);
     return sum;
 }
 
-const numbers: number[] = [1, 3, 5, 7, 9];
-console.log('Сума чисел:', sumArray(numbers));
+console.log('[1, 2, 3, 4]:', sumArray([1, 2, 3, 4]));
+console.log('["1", "2", "3", "4"]:', sumArray(['1', '2', '3', '4']));
+console.log('[]:', sumArray([]));
+console.log('["a", "b", "c", "d"]:', sumArray(['a', 'b', 'c', '4']));
 
-const strings: string[] = ['4', '6', '8', '12'];
-console.log('Сума чисел:', sumArray(strings));
 
-//2
-
+// 2
 console.log('Стрілкова функція:');
+const sumArray2 = (arr: number[] | string[]): number | string => {
+    if (arr.length === 0) {
+        console.log('Масив порожній, повертаємо 0');
+        return 0;
+    }
 
-const sumArray2 = (arr: (number | string)[]): number => {
-    let sum = 0;
-    arr.forEach((Element) => {
-        sum += Number(Element);
+    let canConvertAllToNumbers = true;
+    arr.forEach(
+        el => {
+            if (isNaN(Number(el))) {
+                canConvertAllToNumbers = false;
+            }
+        }
+    );
+
+    if (canConvertAllToNumbers) {
+        let sum = 0;
+        arr.forEach(num => {
+            sum += Number(num);
+        });
+        console.log('Усі елементи числові. Сума елементів:', sum);
+        return sum;
+    };
+    let sum = '';
+    arr.forEach(num => {
+        sum += String(num);
     });
+    console.log('Знайдено нечислові елементи. Конкатенуємо всі елементи:', sum);
     return sum;
 };
 
-const numbers2: number[] = [1, 3, 5, 7, 9];
-console.log('Сума чисел:', sumArray2(numbers2));
 
-const strings2: string[] = ['4', '6', '8', '12'];
-console.log('Сума чисел:', sumArray2(strings2));
+console.log('[1, 2, 3, 4]:', sumArray2([1, 2, 3, 4]));
+console.log('["1", "2", "3", "4"]:', sumArray2(['1', '2', '3', '4']));
+console.log('[]:', sumArray2([]));
+console.log('["a", "b", "c", "4"]:', sumArray2(['a', 'b', 'c', '4']));
