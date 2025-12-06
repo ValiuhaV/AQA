2f9dce11ad892a6105349f10ef5d1e090072f0b1
Valentyna Valiuk <valiuk.gt@gmail.com>
2025-12-06 12:09:05 +0200

    updated playwright configs lesson 18

--- lesson18/playwright.config.ts
+++ lesson18/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'list',
+    reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
@@ -38,7 +38,7 @@ export default defineConfig({
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
-                headless: false
+                headless: true
             }
         }
 
